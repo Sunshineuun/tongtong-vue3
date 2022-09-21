a24f5329678bd4f626028f132f313a77e214a777
qiushengming <qiushengming@aliyun.com>
2022-09-21 11:41:26 +0800

    1. 路由配置 2. svg配置 3. Element Plus配置

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import router from './router'
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
+import '@/styles/index.scss'
+import SvgIcon from '@/icons'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+SvgIcon(app)
+app.use(ElementPlus,{
+    locale:zhCn
+})
+app.use(router).mount('#app')
