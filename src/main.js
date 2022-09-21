d97048d594dc996b6b1bc567021044fd24a4cefc
qiushengming <qiushengming@aliyun.com>
2022-09-21 15:05:42 +0800

    App使用Store

--- src/main.js
+++ src/main.js
@@ -6,10 +6,11 @@ import 'element-plus/dist/index.css';
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import '@/styles/index.scss'
 import SvgIcon from '@/icons'
+import store from './store'
 
 const app = createApp(App)
 SvgIcon(app)
-app.use(ElementPlus,{
-    locale:zhCn
+app.use(ElementPlus, {
+    locale: zhCn
 })
-app.use(router).mount('#app')
+app.use(router).use(store).mount('#app')
