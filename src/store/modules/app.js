5be56a30f37592cd5469cfb38ba41401a06616f1
qiushengming <qiushengming@aliyun.com>
2022-09-22 10:49:59 +0800

    fix: tokn >> token。单词拼写错误，导致token获取异常，界面刷新后就获取不到了

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,10 +1,14 @@
 import {login as loginApi} from "@/api/login";
-import router from '@/router'
+import router from '@/router';
+import {setTokenTime} from "@/util/auth";
 
+/**
+ * 这里有个疑问：当界面被刷新后，state。
+ */
 export default {
     namespaced: true,
     state: () => ({
-        toke: localStorage.getItem('token') || ''
+        token: localStorage.getItem('token') || ''
     }),
     mutations: {
         setToken(state, token) {
@@ -19,12 +23,20 @@ export default {
                     .then(res => {
                         console.log(res)
                         commit('setToken', res.token)
+                        setTokenTime();
                         router.replace('/')
                         resolve()
                     }).catch(error => {
                     reject(error)
                 })
             })
+        },
+        logout({commit}) {
+            // clean token
+            commit('setToken', '');
+            localStorage.clear();
+            // 跳转到登录页面
+            router.replace('/login');
         }
     }
 }
