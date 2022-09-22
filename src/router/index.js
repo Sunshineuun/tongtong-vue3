71d7731fd8c3852db35f647ae7a4e3a0507068f4
qiushengming <qiushengming@aliyun.com>
2022-09-22 14:06:46 +0800

    feat: 添加默认路由

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ const routes = [
         path: '/',
         name: '/',
         component: () => import('../layout'),
-        // redirect: '/',
+        redirect: '/users',
         children: [
             {
                 path: 'users',
